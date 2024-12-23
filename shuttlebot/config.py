0f06aeec5e26c719e45fcc09226824a676e4e787
YASIR KHALID <yasir_khalid@outlook.com>
2024-12-23 16:14:27 +0000

    Stop tracking .idea folder

--- shuttlebot/config.py
+++ shuttlebot/config.py
@@ -11,11 +11,6 @@ from enum import Enum
 from uuid import UUID
 
 
-class Parsers(Enum):
-    better = UUID("5184145c-4d83-4e3b-8bed-3466331a45ba")
-    schoolhire = UUID("331b6c8a-ef34-4633-82d9-9b13852117ca")
-
-
 class Location(BaseModel):
     """Location metadata, important for nearby slots searches"""
 
@@ -27,6 +22,16 @@ class Location(BaseModel):
 class SportsCentre(BaseModel):
     """Sports centre list of locations"""
 
+    venue_name: str
+    slug: str
+    organisation_name: Optional[str]
+    organisation_hash: Optional[str]
+    parser_uuid: UUID4
+    location: Location
+
+
+class Traversals(BaseModel):
+    """Sports centre list of locations"""
     venue_name: str
     slug: str
     organisation_name: Optional[str]
